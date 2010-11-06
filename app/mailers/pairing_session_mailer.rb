b0c355dc2b3722e8115cef8c1b9f389129f58f3b
Ben Atkin & Prakash Murthy <pair+benatkin+_prakash@promiscuouspairprogramming.com>
2010-11-06 17:08:05 -0600

    add mail to notify owner about signup by pair

--- app/mailers/pairing_session_mailer.rb
+++ app/mailers/pairing_session_mailer.rb
@@ -1,3 +1,19 @@
 class PairingSessionMailer < ActionMailer::Base
-  default :from => "from@example.com"
+  default :from => "prakblr@yahoo.com"
+  
+  # Mail sent to the owner of the pairing session, informing him/her about the confirmation.
+  def owner_confirm_email(pairing_session)
+    @recipient = pairing_session.owner
+    mail(:to => @recipient.email,
+         :subject => "You have a new pair.")    
+  end
+  
+  # # Mail sent to the person who signs up to a pairing session. 
+  # def pair_confirm_email(pairing_session)
+  #   @recipient = pairing_session.pair
+  #   mail(:to => @recipient.email,
+  #        :subject => "Thanks for signing up to pair.")    
+  #   
+  # end
+  
 end
